9ae1aed5fa2f8c0c18c2be091770056f5d73ea42
Ismail <ismailfadlallaali@gmail.com>
2025-10-11 09:14:51 +0400

    Fix legal pages: add CopyWebpackPlugin to copy legal HTML files to dist, add routes in vercel.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
@@ -36,6 +37,26 @@ module.exports = (env, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'public', 'legal'),
+            to: path.resolve(__dirname, 'dist', 'legal'),
+          },
+          {
+            from: path.resolve(__dirname, 'public', 'images'),
+            to: path.resolve(__dirname, 'dist', 'images'),
+          },
+          {
+            from: path.resolve(__dirname, 'public', 'manifest.json'),
+            to: path.resolve(__dirname, 'dist', 'manifest.json'),
+          },
+          {
+            from: path.resolve(__dirname, 'public', 'favicon.ico'),
+            to: path.resolve(__dirname, 'dist', 'favicon.ico'),
+          }
+        ],
       }),
       new Dotenv({
         path: path.resolve(__dirname, '.env'),
