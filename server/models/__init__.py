c54dd40ae06ed39bd83869c25075ebaa37dfc726
Ismail <ismailfadlallaali@gmail.com>
2025-10-13 13:20:00 +0400

    Fix circular import: Import PiPayment directly in pi.py routes

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -2,6 +2,6 @@
 from .user import User, db
 from .song import Song
 from .user_library import UserLibrary
-from .pi_payment import PiPayment
+# Note: PiPayment imported separately to avoid circular import
 
-__all__ = ['User', 'Song', 'UserLibrary', 'PiPayment', 'db']
+__all__ = ['User', 'Song', 'UserLibrary', 'db']
