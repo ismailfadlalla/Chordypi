6d0f2a7b90fc63551aeb25fffc14ce48a5ec6161
Ismail <ismailfadlallaali@gmail.com>
2025-10-15 07:54:16 +0400

    AGGRESSIVE FIX v1.0.4: Force all containers to top with !important, remove ALL spacing on PlayerPage mobile

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -146,7 +146,7 @@ const Footer = () => {
                         color: '#888',
                         fontSize: '14px'
                     }}>
-                        &copy; {new Date().getFullYear()} ChordyPi. All rights reserved. v1.0.3
+                        &copy; {new Date().getFullYear()} ChordyPi. All rights reserved. v1.0.4
                     </p>
                     <p style={{ 
                         margin: '0',
