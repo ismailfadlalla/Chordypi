86f32d185e7a5eaccf0c58dca7e3239d2e406878
Ismail <ismailfadlallaali@gmail.com>
2026-03-26 11:54:17 +0400

    DEPLOY: Verified commit for Vercel deployment

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
-module.exports = function(api) {
+// Babel configuration for Expo web build
+// Updated: 2026-03-26 - Vercel verified deploymentmodule.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
