8bc1bade50f8eb84401f6a6a5f8efebb5bab10ea
Ismail <ismailfadlallaali@gmail.com>
2026-03-26 13:04:46 +0400

    FIX: Correct babel.config.js syntax for Expo web build

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 // Babel configuration for Expo web build
-// Updated: 2026-03-26 - Vercel verified deploymentmodule.exports = function(api) {
+// Updated: 2026-03-26 - Vercel verified deployment
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: []
+    plugins: [],
   };
 };
